34012cf79023701ac896cc69f7f82789771c6a68
AlfredoSeguraVara <pixxo2010@gmail.com>
2025-11-25 11:55:01 -0600

    feat ✨ se crea el archivo de rutas
    #Que se hizo:
    Se crea un archivo base para el manejo de rutas y metodos basicos get,post,put y delete
    #Impacto:
    Permite un solo canal de rutas para su posterior mejora. Esto permite apliar una capa de seguridad y ademas de limpieza en la arquitectura de la aplicacion. Manejado las mejores practicas posibles.

--- routes/web.php
+++ routes/web.php
@@ -1,2 +1,44 @@
 <?php
 
+$routes = array_filter(explode('/', $_SERVER['REQUEST_URI']));
+$json=[];
+
+if (empty($routes)){
+    $json=[
+        'status' => 404,
+        'result'=>'Not found'
+        ];
+
+    echo json_encode($json,http_response_code($json['status']));
+    return;
+}
+
+if (isset($_SERVER['REQUEST_METHOD']))
+{
+    switch ($_SERVER['REQUEST_METHOD']) {
+        case 'GET':
+            $json=[
+                'status' => 200,
+                'result'=>'GET'
+            ];
+            break;
+        case 'POST':
+            $json=[
+                'status' => 200,
+                'result'=>'POST'
+            ];
+            break;
+        case 'PUT':
+            $json=[
+                'status' => 200,
+                'result'=>'PUT'
+            ];
+            break;
+        case 'DELETE':
+            $json=[
+                'status' => 200,
+                'result'=>'DELETE'
+            ];
+    }
+    echo json_encode($json,http_response_code($json['status']));
+}
