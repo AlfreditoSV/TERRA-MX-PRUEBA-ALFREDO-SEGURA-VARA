d9a0fedc8f0dcb28e56138934666258222c86852
AlfredoSeguraVara <pixxo2010@gmail.com>
2025-11-25 21:25:37 -0600

    feat ✨ se crean migracion de la tabla task
    #Que se hizo:
    Se crea la migracion de la tabla TASKS, esto generando un archivo de migracion para su controls del proyecto
    #Impacto:
    Permite tener una documentacion y consultas en el proyecto para identificar las tablas y columnas de proyecto, ademas de un control de despliegue

--- database/migration/25112025_table_tasks_create.php
+++ database/migration/25112025_table_tasks_create.php
@@ -1,5 +1,27 @@
 <?php
 
-class TableTasksCreate{
+include_once __DIR__.'/../../config/connection_database.php';
+class TableTasksCreate extends ConnectionDatabase {
+
+
+    public function create()
+    {
+        $connect = self::connection();
+        try {
+            $sql = 'CREATE TABLE IF NOT EXISTS tasks(
+    id INT NOT NULL AUTO_INCREMENT,
+    task_name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY(id),
+    INDEX (task_name (10))
+    )';
+            $connect->exec($sql);
+
+        }catch (Exception $e){
+            die ('Error:'.$e->getMessage());
+        }
+
+    }
 
 }
--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', '/php_errors.log');
 
+//Conexion a base de datos
+require_once __DIR__ . '/../config/connection_database.php';
+$connect=ConnectionDatabase::connection();
+$migration=ConnectionDatabase::migration();
+
 
 //Manejo de rutas
 require_once __DIR__.'/../app/controllers/RoutesController.php';
