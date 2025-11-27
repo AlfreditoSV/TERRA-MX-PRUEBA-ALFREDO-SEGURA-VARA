eecb083c4c530511c88aa7ea6ac5b943c82e5ff4
AlfredoSeguraVara <pixxo2010@gmail.com>
2025-11-26 22:18:03 -0600

    feat ✨ se crean comentarios de servicios delete y get
    #Que se hizo:
    Se crean los comentarios para identificar los procesos
    
    #Impacto:
    Permite mejor interpretacion de las funciones

--- routes/services/delete.php
+++ routes/services/delete.php
@@ -1,3 +1,4 @@
 <?php
 
-
+//Ruta para manejar la peticion DELETE /tasks/{id}
+if (count($routes) == 3 && $routes[1] == 'tasks' && $routes[2] == 'delete') return (new TaskController())->delete($routes[3]);
--- routes/services/get.php
+++ routes/services/get.php
@@ -1,8 +1,5 @@
 <?php
 
-include __DIR__.'/../../app/controllers/TaskController.php';
-
-
-if(count($routes)==1 && $routes[1]=='tasks') return (new TaskController())->index();
-if(count($routes)==2 && $routes[1]=='tasks') return (new TaskController())->show($routes[2]);
-
+//Ruta para manejar la peticion GET /tasks y /tasks/{id}
+if (count($routes) == 1 && $routes[1] == 'tasks') return (new TaskController())->index();
+if (count($routes) == 2 && $routes[1] == 'tasks' && $routes[2] != 'add') return (new TaskController())->show($routes[2]);
