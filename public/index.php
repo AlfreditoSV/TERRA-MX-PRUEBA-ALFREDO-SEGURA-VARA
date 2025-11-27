862959f0dadfc12b4e9ae33d15e7a5c8727d09a1
AlfredoSeguraVara <pixxo2010@gmail.com>
2025-11-26 21:12:33 -0600

    feat ✨ se crean headers en archivo de entrada
    #Que se hizo:
    Se crean los headers en el archivos public/index.php. para permitir cualquier conexion, limitar los metodos, el uso con caracteres especiales en respuestas json y el tiempo de contenido html y no texto plano.
    #Impacto:
    Permite crear una capa de seguridad basica de seguridad en las solicitudes entrantes y salientes.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,17 @@
 <?php
 
 //Cacheo de errores
-
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', '/php_errors.log');
+
+//Cabeceras
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: origin, x-requested-with, content-type');
+header('content-type: application/json; charset=utf-8');
+header('Content-Type: text/html; charset=utf-8');
+
 
 //Conexion a base de datos
 require_once __DIR__ . '/../config/connection_database.php';
