bb8ea5c192db10834f08c1e73fdd1989039beadf
AlfredoSeguraVara <pixxo2010@gmail.com>
2025-11-26 21:28:31 -0600

    feat ✨ se crea el servicio post para las rutas
    #Que se hizo:
    Se crea el archivo routes/servicies/post para el manejo de peticiones http tipo POST
    #Impacto:
    Permite filtrar  las entradas de tipo POST el cual permite restringir solo las rutas dadas de alta en el sistema.

--- routes/services/post.php
+++ routes/services/post.php
@@ -1,1 +1,7 @@
 <?php
+
+
+//Ruta para manejar la peticion POST /tasks
+if (isset($_POST)) {
+    if (count($routes) == 2 && $routes[1] == 'tasks' && $routes[2] == 'store') return (new TaskController())->store($_POST);
+}
